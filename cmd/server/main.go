a72b166d22e54a23b233ded2d4c3f39602bb916d
MinhT <phammanhtoanhht933@gmail.conm>
2025-07-22 10:22:07 +0700

    refactor: clean up import order and update host in API documentation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"log"
+
 	_ "github.com/MinhT933/file-converter/docs"
 	"github.com/MinhT933/file-converter/internal/api"
 	"github.com/MinhT933/file-converter/internal/config"
@@ -9,7 +11,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/hibiken/asynq"
-	"log"
 )
 
 // @title           File Converter API
@@ -17,7 +18,7 @@ import (
 // @description     Upload & convert files asynchronously via Asynq queue.
 // @contact.name    minht
 // @contact.email   phammanhtoanhht933@gmail.com
-// @host      127.0.0.1:8080
+// @host      localhost:8080
 // @schemes   https
 // @BasePath  /api
 func main() {
@@ -34,28 +35,26 @@ func main() {
 		BodyLimit: cfg.MaxUploadMB * 1024 * 1024,
 	})
 
-
 	//thêm
 	app.Use(cors.New(cors.Config{
-	    AllowOrigins: "http://127.0.0.1:8080, http://localhost:8080",
+		AllowOrigins:     "http://127.0.0.1:8080, http://localhost:8080",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowCredentials: true,
-        AllowHeaders:     "Content-Type, Authorization",
-		ExposeHeaders: "Content-Disposition",
+		AllowHeaders:     "Content-Type, Authorization",
+		ExposeHeaders:    "Content-Disposition",
 	}))
 
-	app.Use(cors.New()) 
+	app.Use(cors.New())
 
 	app.Get("/swagger/*", fiberSwagger.HandlerDefault)
 
 	api.RegisterRoutes(app, cfg, asynqClient)
 
-    log.Fatal(app.ListenTLS(
-    ":8080",
-    "127.0.0.1.pem",
-    "127.0.0.1-key.pem",
-   ))
-
+	log.Fatal(app.ListenTLS(
+		":8080",
+		"127.0.0.1.pem",
+		"127.0.0.1-key.pem",
+	))
 
 	if err := app.Listen(":" + cfg.PortHTTP); err != nil {
 		panic(err)
