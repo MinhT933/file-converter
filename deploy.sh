e653df35083f85e309106175e45648c57a57abbe
MinhT <phammanhtoanhht933@gmail.conm>
2025-09-01 18:00:11 +0700

    fix: simplify deployment script by removing old image logic and updating pull command

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,19 @@ cd "$STACK_DIR" || { echo "❌ STACK_DIR=$STACK_DIR not found"; exit 1; }
 echo "==> Stop old containers..."
 docker compose -f docker-compose.prod.yml down || true
 
-echo "==> Remove old image..."
-for repo in "192.168.1.100:5001/$IMAGE_NAME_SERVER" "192.168.1.100:5001/$IMAGE_NAME_WORKER"; do
-  docker images --format '{{.Repository}}:{{.Tag}}' \
-  | awk -v repo="$repo" -v keep="$TAG" \
-      'index($0, repo ":")==1 && $0 != (repo ":" keep) && $0 != (repo ":latest") {print}' \
-  | xargs -r -n1 docker rmi -f || true
-done
+# echo "==> Remove old image..."
+# for repo in "192.168.1.100:5001/$IMAGE_NAME_SERVER" "192.168.1.100:5001/$IMAGE_NAME_WORKER"; do
+#   docker images --format '{{.Repository}}:{{.Tag}}' \
+#   | awk -v repo="$repo" -v keep="$TAG" \
+#       'index($0, repo ":")==1 && $0 != (repo ":" keep) && $0 != (repo ":latest") {print}' \
+#   | xargs -r -n1 docker rmi -f || true
+# done
 
-echo "==> Update server & worker..."
-docker compose -f docker-compose.prod.yml pull server worker
+echo "==> Pull latest image..."
+docker compose -f docker-compose.prod.yml pull
 
 echo "==> Start new containers..."
-docker compose -f docker-compose.prod.yml up -d --no-deps --force-recreate --remove-orphans server worker
+docker compose -f docker-compose.prod.yml up -d --remove-orphans
 
 
 echo "==> Current status..."
